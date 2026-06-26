525e036c6989f8a1de40fa02057c23a0a4be93d0
Emil Aby <emilaby123@gmail.com>
2026-06-26 17:17:23 +0100

    Adjusted padding when "Awaiting data" status displayed in LastSessionCard

--- components/LastSessionCard.tsx
+++ components/LastSessionCard.tsx
@@ -94,7 +94,7 @@ export default function LastSessionCard (){
             <p className="text-xs font-semibold text-gray-500 mb-2">LAST SESSION</p>
             <h1 className="font-semibold text-sm lg:text-lg mb-2 lg:mt-2">{sessionInfo.name}</h1>
             {sessionData.length === 0 && 
-            <div className="flex flex-col items-center text-center mt-4">
+            <div className="flex flex-col items-center text-center mt-4 lg:mb-2">
                 <h2 className="font-bold text-lg lg:text-2xl mb-3 mt-1 animate-pulse">Awaiting latest session data...</h2>
                 <p className="italic text-xs lg:text-sm text-gray-500">Data may be delayed by ~2.5 hours due to OpenF1 processing.</p>
             </div>}
